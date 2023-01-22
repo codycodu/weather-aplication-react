33c5df0ed9c15596a90d5dd8c245a9626928a0fc
codycodu <codydenisa16@gmail.com>
2023-01-22 18:04:17 +0000

    Added GitHub and portofolio url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://www.codruta-blaga.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Codruta Blaga
+        </a>
+        and is
+        <a
+          href="https://github.com/codycodu/weather-aplication-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+        </a>
+        open-sourced on GitHub
+      </footer>
     </div>
   );
 }
