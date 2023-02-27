4947595402cabfb7a7bbec5402b7b8b98fb6d562
codycodu <codydenisa16@gmail.com>
2023-02-27 17:51:52 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by {""}
-        <a
-          href="https://www.codruta-blaga.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Codruta Blaga {""}
-        </a>
-        and is {""}
-        <a
-          href="https://github.com/codycodu/weather-aplication-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div class="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by {""}
+          <a
+            href="https://www.codruta-blaga.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Codruta Blaga {""}
+          </a>
+          and is {""}
+          <a
+            href="https://github.com/codycodu/weather-aplication-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
